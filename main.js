d98c363e2315b42b7b12ea498040e8e85e1b4f34
rishavdhungel <rishavdhungel3@gmail.com>
2023-07-29 17:59:20 +0545

    power function implemented.

--- main.js
+++ main.js
@@ -3,30 +3,66 @@
 //use output screen to output result
 
 const keys = document.querySelector('#power');
+const operations = document.getElementsByClassName("operators");
+const numbers = document.getElementsByClassName("number");
 const output = document.querySelector(".outputdata")
 let power = 0;
+let firstNumber;
+let secondNumber;
+let operator;
+
 
 keys.addEventListener("click", function(){
-    
-    if(power % 2 === 0){
+    if(power === 0){
         console.log("Calculator is on");
         //activate
         console.log(power)
         output.innerText =0;
-        evaluate();
         power += 1;
+        listentokey();
     }else{
-        power +=1;
         console.log("calculator is off");
+        console.log(power);
         output.innerText = "";
+        power -= 1;
     }
 }
 );
 
 
 
-function evaluate(){
+function operate(firstNumber,operator,secondNumber){
+    //now we perform a switch case logic to perform each operation
+    
+    switch(operator){
+        case '*':
+            console.log("call multiply function here");
+            break;
+        case '+':
+            console.log('call addition function here');
+            break;
+        case '/':
+            console.log('call division function here');
+            break;
+        case '-':
+            console.log("call the substraction function here");
+            break;
+        default:
+            alert("Happy Hacking!");
+        
+    }
 
-    console.log("Calculating");
+}
 
+
+
+function listentokey(){
+    console.log("function called");
+    for (let i = 0; i<operations.length; i++){
+        operations[i].addEventListener('click', function(){
+           let Symbol =  operations[i].getAttribute('data-operator');
+           console.log(Symbol);
+           operate(0,Symbol,0);
+        })
+    }
 }
